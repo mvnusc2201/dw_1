8e6d118c47c36ac9e0f5f60c3c84ab5f1a4f730f
Cristopher Pradenas <Cristxpherjesus23@gmail.com>
2026-09-20 18:29:38 -0300

    Semana 7: Maquetación HTML estática y CSS  finalizados

--- Semana_7/Entrega_Frontend/src/data/deliveryApps.js
+++
@@ -1,8 +0,0 @@
-import { images } from './images.js';
-
-/** Third party delivery partners. */
-export const deliveryApps = [
-  { id: 'pedidosya', name: 'PedidosYa', image: images.pedidosYa, href: 'https://www.pedidosya.cl' },
-  { id: 'ubereats', name: 'Uber Eats', image: images.uberEats, href: 'https://www.ubereats.com' },
-  { id: 'rappi', name: 'Rappi', image: images.rappi, href: 'https://www.rappi.cl' },
-];
